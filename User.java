c20e3a60f0a1e16ad9f23747b1cb128252b26e21
Itarx <natthapong_nar@cmu.ac.th>
2026-01-09 16:11:04 +0700

    Create User.java

---
+++ User.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+
+public class User {
+    protected String name ;
+    protected LocalDate dob ;
+    protected  LocalDate today = LocalDate.now() ;
+
+    public User() {
+        this.name = null ;
+        this.dob = LocalDate.now()  ; // current date
+    }
+
+    public User(String name , int year , int month , int day) {
+        this.name = name ;
+        this.dob = LocalDate.of (year , month , day) ;
+    }
+
+    Boolean isBirthday() {
+        LocalDate today = LocalDate.now() ;
+        return today.getMonth() == dob.getMonth() && today.getDayOfMonth() == dob.getDayOfMonth() ;
+    }
+
+    public void displayHappyBirthday() {
+        if(isBirthday())
+            System.out.println("Happy birthday " + this.name + "!");
+    }
+
+
+
+    void displayInfo() {
+        System.out.println("Name : " +name );
+        System.out.println(name + " was born on " + dob );
+    }
+}
